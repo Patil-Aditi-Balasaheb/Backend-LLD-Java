61c371d75caf3f021740fb28c7b571647e92b5fd
Aditi Patil <52288839+Patil-Aditi-Balasaheb@users.noreply.github.com>
2024-12-26 21:46:01 +0530

    Day 3 - Java Advanced Concepts 2 - Collections (Milestone 1 - Item Class Hierarchy)

---
+++ src/lab/day3_java_advanced_concepts_2_collections/ecommerce_inventory_management_system/Book.java
@@ -0,0 +1,18 @@
+package lab.day3_java_advanced_concepts_2_collections.ecommerce_inventory_management_system;
+
+public class Book extends Item {
+    private String author;
+
+    public Book(String id, String name, double price, int quantity, String author) {
+        super(id, name, price, quantity);
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
