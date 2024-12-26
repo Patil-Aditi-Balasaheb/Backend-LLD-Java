fded587e99f60b479dfe466580d3fd3e3f1959ac
Aditi Patil <52288839+Patil-Aditi-Balasaheb@users.noreply.github.com>
2024-12-27 01:18:08 +0530

    Day 3 - Java Advanced Concepts 2 - Collections (Milestone 3 - Recently Viewed Items)

--- src/lab/day3_java_advanced_concepts_2_collections/ecommerce_inventory_management_system/Inventory.java
+++ src/lab/day3_java_advanced_concepts_2_collections/ecommerce_inventory_management_system/Inventory.java
@@ -2,13 +2,16 @@ package lab.day3_java_advanced_concepts_2_collections.ecommerce_inventory_manage
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Inventory<T extends Item> {
     private HashMap<String, T> ids;
+    public LinkedList<Item> recentViewedItems;
 
     public Inventory() {
         ids = new HashMap<>();
+        recentViewedItems = new LinkedList<>();
     }
 
     public void addItem(T item) {
@@ -20,10 +23,18 @@ public class Inventory<T extends Item> {
     }
 
     public T getItem(String id) {
+        addRecentViewedItem(ids.get(id));
         return ids.get(id);
     }
 
     public List<T> getAllItems() {
         return new ArrayList<>(ids.values());
+    }
+
+    public void addRecentViewedItem(Item item) {
+        if (recentViewedItems.size() >= 3) {
+            recentViewedItems.removeFirst();
+        }
+        recentViewedItems.add(item);
     }
 }
--- src/lab/day3_java_advanced_concepts_2_collections/ecommerce_inventory_management_system/Main.java
+++ src/lab/day3_java_advanced_concepts_2_collections/ecommerce_inventory_management_system/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Item i2 = new Item("2", "Macbook", 500.34, 10);
         Item i3 = new Item("3", "IPhone", 100.37, 50);
 
-        Inventory<Item> inventory = new Inventory();
+        Inventory<Item> inventory = new Inventory<Item>();
 
         // add items to the inventory
         inventory.addItem(i1);
@@ -17,6 +17,13 @@ public class Main {
         System.out.println("ID: " + i1.getId() + ", Name: " + inventory.getItem(i1.getId()).getName());
         System.out.println("ID: " + i2.getId() + ", Name: " + inventory.getItem(i2.getId()).getName());
         System.out.println("ID: " + i3.getId() + ", Name: " + inventory.getItem(i3.getId()).getName());
+        System.out.println("ID: " + i2.getId() + ", Name: " + inventory.getItem(i2.getId()).getName());
+
+        // print recently viewed list
+        System.out.println("\n\nRecently Viewed: ");
+        for (Item i : inventory.recentViewedItems) {
+            System.out.println("ID: " + i.getId() + ", Name: " + i.getName());
+        }
 
         // remove an item from inventory by id
         inventory.removeItem(i2.getId());
@@ -26,7 +33,13 @@ public class Main {
         inventory.addItem(i4);
 
         // get the item name of the duplicate item from inventory
-        System.out.println("ID: " + i4.getId() + ", Name: " + inventory.getItem(i4.getId()).getName());
+        System.out.println("\n\nID: " + i4.getId() + ", Name: " + inventory.getItem(i4.getId()).getName());
         System.out.println("ID: " + i1.getId() + ", Name: " + inventory.getItem(i1.getId()).getName());
+
+        // print recently viewed list
+        System.out.println("\n\nRecently Viewed: ");
+        for (Item i : inventory.recentViewedItems) {
+            System.out.println("ID: " + i.getId() + ", Name: " + i.getName());
+        }
     }
 }
